df42172238be665f8beaa77e4ef97a55e9e07e73
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2022-11-18 09:31:11 -0500

    [SOL] Make adjustments after upgrading to llvm 15.0

--- lld/ELF/Arch/BPF.cpp
+++ lld/ELF/Arch/BPF.cpp
@@ -1,4 +1,4 @@
-//===- SBF.cpp ------------------------------------------------------------===//
+//===- BPF.cpp ------------------------------------------------------------===//
 //
 //                             The LLVM Linker
 //
@@ -23,9 +23,9 @@ namespace lld {
 namespace elf {
 
 namespace {
-class SBF final : public TargetInfo {
+class BPF final : public TargetInfo {
 public:
-  SBF();
+  BPF();
   RelExpr getRelExpr(RelType type, const Symbol &s,
                      const uint8_t *loc) const override;
   RelType getDynRel(RelType type) const override;
@@ -35,20 +35,20 @@ public:
 };
 } // namespace
 
-SBF::SBF(Ctx &ctx) : TargetInfo(ctx) {
-  relativeRel = R_SBF_64_RELATIVE;
-  symbolicRel = R_SBF_64_64;
+BPF::BPF() {
+  relativeRel = R_BPF_64_RELATIVE;
+  symbolicRel = R_BPF_64_64;
 }
 
-RelExpr SBF::getRelExpr(RelType type, const Symbol &s,
+RelExpr BPF::getRelExpr(RelType type, const Symbol &s,
                         const uint8_t *loc) const {
   switch (type) {
-    case R_SBF_64_32:
+    case R_BPF_64_32:
       return R_PC;
-    case R_SBF_64_ABS32:
-    case R_SBF_64_NODYLD32:
-    case R_SBF_64_ABS64:
-    case R_SBF_64_64:
+    case R_BPF_64_ABS32:
+    case R_BPF_64_NODYLD32:
+    case R_BPF_64_ABS64:
+    case R_BPF_64_64:
       return R_ABS;
     default:
       error(getErrorLocation(loc) + "unrecognized reloc " + toString(type));
@@ -56,39 +56,39 @@ RelExpr SBF::getRelExpr(RelType type, const Symbol &s,
   return R_NONE;
 }
 
-RelType SBF::getDynRel(RelType type) const {
+RelType BPF::getDynRel(RelType type) const {
   switch (type) {
-    case R_SBF_64_ABS64:
-        // R_SBF_64_ABS64 is symbolic like R_SBF_64_64, which is set as our
-        // symbolicRel in the constructor. Return R_SBF_64_64 here so that if
+    case R_BPF_64_ABS64:
+        // R_BPF_64_ABS64 is symbolic like R_BPF_64_64, which is set as our
+        // symbolicRel in the constructor. Return R_BPF_64_64 here so that if
         // the symbol isn't preemptible, we emit a _RELATIVE relocation instead
         // and skip emitting the symbol.
         //
         // See https://github.com/solana-labs/llvm-project/blob/6b6aef5dbacef31a3c7b3a54f7f1ba54cafc7077/lld/ELF/Relocations.cpp#L1179
-        return R_SBF_64_64;
+        return R_BPF_64_64;
     default:
         return type;
   }
 }
 
-int64_t SBF::getImplicitAddend(const uint8_t *buf, RelType type) const {
+int64_t BPF::getImplicitAddend(const uint8_t *buf, RelType type) const {
   return 0;
 }
 
-void SBF::relocate(uint8_t *loc, const Relocation &rel, uint64_t val) const {
+void BPF::relocate(uint8_t *loc, const Relocation &rel, uint64_t val) const {
   switch (rel.type) {
-    case R_SBF_64_32: {
+    case R_BPF_64_32: {
       // Relocation of a symbol
       write32le(loc + 4, ((val - 8) / 8) & 0xFFFFFFFF);
       break;
     }
-    case R_SBF_64_ABS32:
-    case R_SBF_64_NODYLD32: {
+    case R_BPF_64_ABS32:
+    case R_BPF_64_NODYLD32: {
       // Relocation used by .BTF.ext and DWARF
       write32le(loc, val & 0xFFFFFFFF);
       break;
     }
-    case R_SBF_64_64: {
+    case R_BPF_64_64: {
       // Relocation of a lddw instruction
       // 64 bit address is divided into the imm of this and the following
       // instructions, lower 32 first.
@@ -96,7 +96,7 @@ void SBF::relocate(uint8_t *loc, const Relocation &rel, uint64_t val) const {
       write32le(loc + 8 + 4, val >> 32);
       break;
     }
-    case R_SBF_64_ABS64: {
+    case R_BPF_64_ABS64: {
       // The relocation type is used for normal 64-bit data. The
       // actual to-be-relocated data is stored at r_offset and the
       // read/write data bitsize is 64 (8 bytes). The relocation can
@@ -115,7 +115,7 @@ static uint32_t getEFlags(InputFile *file) {
   return cast<ObjFile<ELF64LE>>(file)->getObj().getHeader().e_flags;
 }
 
-uint32_t SBF::calcEFlags() const {
+uint32_t BPF::calcEFlags() const {
   uint32_t ret = 0;
 
   // Ensure that all the object files were compiled with the same flags, as
@@ -132,8 +132,9 @@ uint32_t SBF::calcEFlags() const {
   return ret;
 }
 
-void setSBFTargetInfo(Ctx &ctx) {
-  ctx.target.reset(new SBF(ctx));
+TargetInfo *getBPFTargetInfo() {
+  static BPF target;
+  return &target;
 }
 
 } // namespace elf
